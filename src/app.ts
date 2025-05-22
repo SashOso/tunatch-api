2458092d9e8307db3d8a5e0b5e84be8827bab12c
andrestuestach <andrestuesta_22@outlook.com>
2025-05-22 10:09:55 -0500

    .

--- src/app.ts
+++ src/app.ts
@@ -6,15 +6,18 @@ const app=express();
 
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.static('uploads'));
+app.use('/upload', express.static('uploads'));
 app.use(cors(
     {
         origin: 'http://localhost:4200',
-        methods: ['GET', 'POST', 'PUT', 'DELETE'],
-        credentials: true
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization', 'Range', 'Accept', 'X-Requested-With'],
+        credentials: true,
+        exposedHeaders: ['Content-Range', 'Accept-Ranges', 'Content-Length', 'Content-Type'],
     }
 ));
 
+"http://localhost:8080/uploads/song/media/Michael Jackson - Billie Jean.mp4"
 
 app.use('/api',routes);
 
